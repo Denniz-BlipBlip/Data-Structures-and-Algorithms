d591bd7153d95c801a15453263f8de1df9373243
Denniz <gabrieldenniz@gmail.com>
2026-06-17 13:55:33 +0800

    maybe I'll add something later

--- Binary-Search-Tree/BST-CPP/Tree.h
+++ Binary-Search-Tree/BST-CPP/Tree.h
@@ -1,15 +1,17 @@
 #pragma once
+#include "Data.h"
 #include "Node.h"
 #include<iostream>
 
 class Tree 
 {
   public:
-    Tree():root(nullptr);
+
+    Tree():root(nullptr){}
 
     void insert(std::string word,int &id)
     {
-      this->root=inser(this->root,word,id);
+      this->root=insert(this->root,word,id);
     }
 
     void removeWord(int &id)
@@ -19,7 +21,7 @@ class Tree
 
     void searchWord(int &id)
     {
-      search(thois->root);
+      search(this->root,id);
     }
 
     void inorder()
@@ -29,4 +31,94 @@ class Tree
 
   private:
 
+    Node *insert(Node *root,std::string word,int &id)
+    {
+      if(!root){return new Node(new Data(word,id));}
+      if(id>root->getData()->getId())
+      {
+        return root->setRight(insert(root->getRight(),word,id));
+      }
+      else 
+      {
+        return root->setLeft(insert(root->getLeft(),word,id));
+      }
+    }
+
+    void inorder(Node *root)
+    {
+      if(!root){return;}
+      inorder(root->getLeft());
+      std::cout<<root->getData()->getWord()<<std::endl;
+      inorder(root->getRight());
+    }
+
+    void search(Node *root,int &id)
+    {
+      if(!root){return;}
+      if(id>root->getData()->getId())
+      {
+        return search(root->getRight(),id);
+      }
+      else if(id<root->getData()->getId())
+      {
+        return search(root->getLeft(),id);
+      }
+      else 
+      {
+        std::cout<<root->getData()->getWord()<<std::endl;
+      }
+    }
+
+    Node *deleteNode(Node *root,int &id)
+    {
+      if(!root){return nullptr;}
+      if(id>root->getData()->getId())
+      {
+        return root->setRight(deleteNode(root->getRight(),id));
+      }
+      else if(id<root->getData()->getId())
+      {
+        return root->setLeft(deleteNode(root->getLeft(),id));
+      }
+      else 
+      {
+        if(!root->getRight()&&root->getLeft())
+        {
+          return nullptr;
+        }
+        else if(!root->getRight())
+        {
+          Node *temp=root->getLeft();
+          delete root->getData();
+          delete root;
+          return temp;
+        }
+        else if(!root->getLeft())
+        {
+          Node *temp=root->getRight();
+          delete root->getData();
+          delete root;
+          return temp;
+        }
+        else 
+        {
+          Node *temp=findMin(root->getRight());
+          root->getData()->setWord(temp->getData()->getWord());
+          root->getData()->setId(temp->getData()->getId());
+          return root->setRight(deleteNode(root->getRight(),id));
+        }
+      }
+    }
+
+    Node *findMin(Node *root)
+    {
+      if(!root){return nullptr;}
+      while(root&&root->getLeft())
+      {
+        root=root->getLeft();
+      }
+      return root;
+    }
+
+    Node *root;
 };
