c19e6df68780ab75fd9f9596cd57cdf9d8691fb1
Denniz <gabrieldenniz@gmail.com>
2026-06-29 15:07:29 +0800

    Another sorting

--- SelectionSort/SelctionSort-Java/src/App.java
+++ SelectionSort/SelctionSort-Java/src/App.java
@@ -1,5 +1,18 @@
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello from SelctionSort-Java!");
+import Sort.Sort;
+
+public class App 
+{
+  public static void main(String[] args)
+  {
+    Sort sort=new Sort();
+
+    int[] arr={9,8,7,6,5,4,3,2,1};
+
+    sort.selection_sort(arr);
+
+    for(int i:arr)
+    {
+      System.out.print(i);
     }
+  }
 }
--- SelectionSort/SelctionSort-Java/src/Sort/Sort.java
+++ SelectionSort/SelctionSort-Java/src/Sort/Sort.java
@@ -2,5 +2,23 @@ package Sort;
 
 public class Sort 
 {
+  public void selection_sort(int[] arr)
+  {
+    int len=arr.length;
 
+    for(int i=0;i<len;i++)
+    {
+      int minIndex=i;
+      for(int j=i+1;j<len;j++)
+      {
+        if(arr[j]<arr[minIndex])
+        {
+          minIndex=j;
+        }
+      }
+      int temp=arr[i];
+      arr[i]=arr[minIndex];
+      arr[minIndex]=temp;
+    }
+  }
 }
