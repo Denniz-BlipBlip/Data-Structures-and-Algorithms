bf445c721172640b179f3532dcf2777252d05402
Denniz <gabrieldenniz@gmail.com>
2026-07-07 13:01:00 +0800

    not yet

--- LinkedList/LinkedList-Csharp/src/Link.cs
+++ LinkedList/LinkedList-Csharp/src/Link.cs
@@ -15,9 +15,41 @@ namespace Data
 
       if(isEmpty())
       {
-
+        this.top=newNode;
+        this.bottom=newNode;
       }
+      else
+      {
+        this.bottom.next=newNode;
+        this.bottom=newNode;
+      }
+      this.size++;
     }
+
+    public void addTop(string data)
+    {
+      Node newNode=new Node(new Data(data));
+
+      if(isEmpty())
+      {
+        this.bottom=null;
+        this.top=null;
+      }
+      else 
+      {
+        newNode.next=this.top;
+        this.top-newNode;
+      }
+      this.size+;
+    }
+
+    public void view()
+    {
+      if(isEmpty()){return;}
+
+    }
+
+    private bool isEmpty(){return this.top==null;}
 
     private Node top,bottom;
     private int size;
