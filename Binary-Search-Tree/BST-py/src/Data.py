ce6208a6dc61524a6f51794e5cf03f97f2252a9a
Denniz <gabrieldenniz@gmail.com>
2026-07-18 13:01:31 +0800

    added BST for python

---
+++ Binary-Search-Tree/BST-py/src/Data.py
@@ -0,0 +1,4 @@
+class Data:
+    def __init__(self,data:Str,id:int):
+        self.data=data
+        self.id=id
---
+++ Binary-Search-Tree/BST-py/src/Tree.py
@@ -0,0 +1,61 @@
+from src import Node
+from src import Data
+
+class Tree:
+    def __init__(self):
+        self.root:Node=None
+
+    def insert(self,data:str,id:int)->None:
+        self.root=self.__insert(self.root,data,id)
+
+    def delete(self,id:int)->None:
+        self.root=self.__delete(self.root,id)
+
+    def view(self)->None:
+        self.__view(self.root)
+
+    def __insert(self,root:Node,data:str,id:int)->Node:
+        if root is None:
+            return Node(Data(data,id))
+
+        if id>root.data.id:
+            return root.right=self.__insert(root.right,data,id)
+        elif id<root.data.id:
+            return root.left=self.__insert(root.left,data,id)
+        else:
+            return None
+
+    def __delete(self,root:Node,id:int)->Node:
+        if root is None:
+            return  None
+
+        if id>root.data.id:
+            return root.right=self.__delete(root.right,id)
+        elif id<root.data.id:
+            return root.left=self.__delete(root.left,id)
+        else:
+            if root.right is None:
+                return root.left
+            elif root.left is None:
+                return root.right
+            else:
+                temp:Node=self.__findMin(root.right)
+                root.data=temp.data
+                return root.right=self.__delete(root.right,temp.data.id)
+
+    def __view(self,root:Node)->None:
+        if root is None:
+            return
+
+        self.__view(root.left)
+        print(root.data.data, end="")
+        self.__view(root.right)
+
+
+    def __findMin(self,root:Node)->Node:
+        if root is None:
+            return None
+
+        while root is not None and root.left is not None:
+            root=root.left
+        return root
