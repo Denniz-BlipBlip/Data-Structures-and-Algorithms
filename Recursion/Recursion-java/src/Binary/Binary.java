ef493030da8670baa79d1b4b65d800f32dbbdd51
Denniz <gabrieldenniz@gmail.com>
2026-09-06 14:06:49 +0800

    added Binary recursion

--- Recursion/Recursion-java/src/Binary/Binary.java
+++ Recursion/Recursion-java/src/Binary/Binary.java
@@ -2,10 +2,19 @@ package Binary;
 
 public class Binary 
 {
-  public int addUpTo(int n)
+  public int fibonacci(int n)
   {
-    if(n<=0)return n;
+    if(n==1||n==0)return n;
 
-    return n+this.addUpTo(n-1);
+    return this.fibonacci(n-1)+this.fibonacci(n-2);
+  }
+
+  public void hanoi(int n,String a,String b,String c)
+  {
+    if(n<=0)return;
+
+    this.hanoi(n-1, a, c, b);
+    System.out.println(" from "+a+" put "+n+" to "+b);
+    this.hanoi(n-1, c, b, a);
   }
 }
